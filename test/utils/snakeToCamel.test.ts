48ad06c1f61f9338f58a881efa989ae9b0fa261d
Enjoywater <heungsoo1@gmail.com>
2023-01-05 11:05:47 +0900

    Fix matchers

--- test/utils/snakeToCamel.test.ts
+++ test/utils/snakeToCamel.test.ts
@@ -2,10 +2,10 @@ import snakeToCamel from '../../src/utils/snakeToCamel';
 
 describe('snake to camel', () => {
   test('one snake', () => {
-    expect(snakeToCamel('user_data')).toEqual('userData');
+    expect(snakeToCamel('user_data')).toBe('userData');
   });
 
   test('two snakes', () => {
-    expect(snakeToCamel('snake_to_camel')).toEqual('snakeToCamel');
+    expect(snakeToCamel('snake_to_camel')).toBe('snakeToCamel');
   });
 });
